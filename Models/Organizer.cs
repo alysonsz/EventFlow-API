5c2147637ce2044ba608e8d7d1a74fc4165cae25
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 08:49:07 -0300

    ✨create 'Organizer' classes

---
+++ Models/Organizer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace EventFlow_API.Models;
+
+public class Organizer
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public ICollection<Event>? Events { get; set; } = new Collection<Event>();
+}
---
+++ Repository/Interfaces/IOrganizerRepository.cs
@@ -0,0 +1,12 @@
+﻿using EventFlow_API.Models;
+
+namespace EventFlow_API.Repository.Interfaces;
+
+public interface IOrganizerRepository
+{
+    Task<Organizer> PostAsync(Organizer organizer);
+    Task<Organizer> UpdateAsync(Organizer organizer);
+    Task<Organizer> DeleteAsync(Organizer organizer);
+    Task<Organizer?> GetOrganizerByIdAsync(int Id);
+    Task<List<Organizer>> GetAllOrganizersAsync(int Id);
+}
