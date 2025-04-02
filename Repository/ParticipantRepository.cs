dcaffb2431747b9be5a297ba0bc00c740e1f2fd0
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 10:05:31 -0300

    ✨create ParticipantRepository

---
+++ Repository/ParticipantRepository.cs
@@ -0,0 +1,42 @@
+﻿using EventFlow_API.Data;
+using EventFlow_API.Models;
+using EventFlow_API.Repository.Interfaces;
+
+namespace EventFlow_API.Repository;
+
+public class ParticipantRepository(EventFlowContext context) : IParticipantRepository
+{
+    public async Task<Participant> PostAsync(Participant participant)
+    {
+        await context.Participant.AddAsync(participant);
+        await context.SaveChangesAsync();
+        return participant;
+    }
+
+    public async Task<Participant> UpdateAsync(Participant participant)
+    {
+        context.Participant.Update(participant);
+        await context.SaveChangesAsync();
+        return participant;
+    }
+
+    public async Task<Participant> DeleteAsync(Participant participant)
+    {
+        context.Participant.Remove(participant);
+        await context.SaveChangesAsync();
+        return participant;
+    }
+
+    public async Task<Participant?> GetParticipantByIdAsync(int id)
+    {
+        return await context.Participant
+            .FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<List<Participant>> GetAllParticipantsAsync(int id)
+    {
+        return await context.Participant
+            .Where(p => p.Id == id)
+            .ToListAsync() ?? new List<Participant>();
+    }
+}
