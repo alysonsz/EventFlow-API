1b817f094c15524a2d060d1cc0d9545637f51a27
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 10:05:15 -0300

    ✨create EventRepository

---
+++ Repository/EventRepository.cs
@@ -0,0 +1,46 @@
+﻿using EventFlow_API.Data;
+using EventFlow_API.Models;
+using EventFlow_API.Repository.Interfaces;
+
+namespace EventFlow_API.Repository;
+
+public class EventRepository(EventFlowContext context) : IEventRepository
+{
+
+    public async Task<Event> PostAsync(Event @event)
+    {
+        await context.Event.AddAsync(@event);
+        await context.SaveChangesAsync();
+        return @event;
+    }
+
+    public async Task<Event> UpdateAsync(Event @event)
+    {
+        context.Event.Update(@event);
+        await context.SaveChangesAsync();
+        return @event;
+    }
+
+    public async Task<int> DeleteAsync(int id)
+    {
+        var eventToDelete = await context.Event.FindAsync(id);
+        if (eventToDelete != null)
+        {
+            context.Event.Remove(eventToDelete);
+            await context.SaveChangesAsync();
+        }
+        return id;
+    }
+
+    public async Task<Event?> GetEventByIdAsync(int id)
+    {
+        return await context.Event
+            .FirstOrDefaultAsync(e => e.Id == id);
+    }
+
+    public async Task<List<Event>> GetAllEventsAsync()
+    {
+        return await context.Event
+            .ToListAsync() ?? new List<Event>();
+    }
+}
