079b3c58920c0a160deed219ec3b9c1ebc14d8a5
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 10:10:05 -0300

    ✨create EventCommand

---
+++ Commands/EventCommand.cs
@@ -0,0 +1,22 @@
+﻿using EventFlow_API.Models;
+using System.Text.Json.Serialization;
+
+namespace EventFlow_API.Commands;
+
+public class EventCommand
+{
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime Date { get; set; }
+    public required string Location { get; set; }
+    public int OrganizerId { get; set; }
+
+    [JsonIgnore]
+    public Organizer Organizer { get; set; }
+
+    [JsonIgnore]
+    public ICollection<Speaker> Speakers { get; set; }
+
+    [JsonIgnore]
+    public ICollection<Participant> Participants { get; set; }
+}
