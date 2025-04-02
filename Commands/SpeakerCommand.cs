9bc629cc5595f121efe7f4a28d63cb28df555bc5
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 10:10:29 -0300

    ✨create SpeakerCommand

---
+++ Commands/SpeakerCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace EventFlow_API.Commands;
+
+public class SpeakerCommand
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public string? Biography { get; set; }
+    public int EventId { get; set; }
+}
