bad8f619a5aa3dec607406df288fb7174db0c055
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 08:49:24 -0300

    ✨create 'Participant' classes

---
+++ Models/Participant.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace EventFlow_API.Models;
+
+public class Participant
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public int EventId { get; set; }
+    public ICollection<Event>? Events { get; set; } = new Collection<Event>();
+}
---
+++ Repository/Interfaces/IParticipantRepository.cs
@@ -0,0 +1,12 @@
+﻿using EventFlow_API.Models;
+
+namespace EventFlow_API.Repository.Interfaces;
+
+public interface IParticipantRepository
+{
+    Task<Participant> PostAsync(Participant participant);
+    Task<Participant> UpdateAsync(Participant participant);
+    Task<Participant> DeleteAsync(Participant participant);
+    Task<Participant?> GetParticipantByIdAsync(int Id);
+    Task<List<Participant>> GetAllParticipantsAsync(int Id);
+}
