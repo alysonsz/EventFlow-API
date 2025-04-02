1caffe27c41e276514acba1bab970ec3940ae6ef
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 10:02:54 -0300

    ✨add DbContext 'EventFlowContext'

---
+++ Data/EventFlowContext.cs
@@ -0,0 +1,23 @@
+﻿using EventFlow_API.Data.Mapping;
+using EventFlow_API.Models;
+
+public class EventFlowContext : DbContext
+{
+    public EventFlowContext(DbContextOptions<EventFlowContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Event> Event { get; set; } = null!;
+    public DbSet<Organizer> Organizer { get; set; } = null!;
+    public DbSet<Participant> Participant { get; set; } = null!;
+    public DbSet<Speaker> Speaker { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new EventMap());
+        builder.ApplyConfiguration(new OrganizerMap());
+        builder.ApplyConfiguration(new ParticipantMap());
+        builder.ApplyConfiguration(new SpeakerMap());
+    }
+}
