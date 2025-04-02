8ca5c66121fb78feaa81160e55dae76ca2d842c9
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 08:49:37 -0300

    ✨create 'Speaker' classes

---
+++ Models/Speaker.cs
@@ -0,0 +1,11 @@
+﻿namespace EventFlow_API.Models;
+
+public class Speaker
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Biography { get; set; }
+    public required string Email { get; set; }
+    public int EventId { get; set; }
+    public Event? Event { get; set; }
+}
---
+++ Repository/Interfaces/ISpeakerRepository.cs
@@ -0,0 +1,12 @@
+﻿using EventFlow_API.Models;
+
+namespace EventFlow_API.Repository.Interfaces;
+
+public interface ISpeakerRepository
+{
+    Task<Speaker> PostAsync(Speaker speaker);
+    Task<Speaker> UpdateAsync(Speaker speaker);
+    Task<Speaker> DeleteAsync(Speaker speaker);
+    Task<Speaker?> GetSpeakerByIdAsync(int id);
+    Task<List<Speaker>> GetAllSpeakersAsync(int id);
+}
