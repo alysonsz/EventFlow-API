c4af0d279a41fda53627171903442259743bca5c
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 10:10:23 -0300

    ✨create ParticipantCommand

---
+++ Commands/ParticipantCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace EventFlow_API.Commands;
+
+public class ParticipantCommand
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public int EventId { get; set; }
+}
