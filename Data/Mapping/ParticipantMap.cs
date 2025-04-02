318f10e5080572806de421e7847bd5f24b4fe866
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-04-02 10:09:09 -0300

    ✨create ParticipantMap
    - Mapping of database fields

---
+++ Data/Mapping/ParticipantMap.cs
@@ -0,0 +1,35 @@
+﻿using EventFlow_API.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventFlow_API.Data.Mapping;
+
+public class ParticipantMap : IEntityTypeConfiguration<Participant>
+{
+    public void Configure(EntityTypeBuilder<Participant> builder)
+    {
+        builder.ToTable("Participant");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnName("Name")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasColumnName("Email")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(150);
+
+        builder
+            .HasMany(p => p.Events)
+            .WithMany(e => e.Participants)
+            .UsingEntity("EventParticipant");
+    }
+}
+
