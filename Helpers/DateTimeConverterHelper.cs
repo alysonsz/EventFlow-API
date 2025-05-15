aed9d47a8cb81163ba796621680fc6cc9dfae9c1
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-05-15 11:09:33 -0300

    ✨feat: add DateTimeConverterHelper for JSON DateTime conversion
    - Create Helpers folder
    - Add JsonConverter<DateTime> to parse and format dates using “dd/MM/yyyy”
    - Provide fallback to ISO parsing and throw descriptive errors on invalid formats

---
+++ Helpers/DateTimeConverterHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EventFlow_API.Helpers;
+
+public class DateTimeConverterHelper : JsonConverter<DateTime>
+{
+    private const string DateFormat = "dd/MM/yyyy";
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.String)
+            throw new JsonException();
+
+        var value = reader.GetString()!;
+        if (DateTime.TryParseExact(value, DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+            return date;
+
+        if (DateTime.TryParse(value, out date))
+            return date;
+
+        throw new JsonException($"Data no formato inválido: {value}. Esperado ISO ou {DateFormat}.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(DateFormat));
+    }
+}
+
