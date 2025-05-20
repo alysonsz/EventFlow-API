5059c2b622f5d2e35311feecf1d62a40938bcf5f
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-05-20 15:23:04 -0300

    ✅feat: add FluentValidation for EventCommand with custom validation rules

---
+++ Validators/EventCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using EventFlow_API.Commands;
+
+namespace EventFlow_API.Validators;
+public class EventCommandValidator : AbstractValidator<EventCommand>
+{
+    public EventCommandValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("O título é obrigatório.")
+            .MaximumLength(200).WithMessage("O título deve ter no máximo 200 caracteres.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(8000).WithMessage("A descrição deve ter no máximo 8000 caracteres.");
+
+        RuleFor(x => x.Date)
+            .GreaterThan(DateTime.Now).WithMessage("A data deve ser no futuro.");
+
+        RuleFor(x => x.Location)
+            .NotEmpty().WithMessage("O local é obrigatório.");
+
+        RuleFor(x => x.OrganizerId)
+            .GreaterThan(0).WithMessage("OrganizerId inválido.");
+    }
+}
