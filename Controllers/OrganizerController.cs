84c27e5e6b040f760de57c3d24cf0107839b5507
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-05-20 14:22:06 -0300

    ♻️refactor: update OrganizerController to use OrganizerService instead of direct repository access

--- Controllers/OrganizerController.cs
+++ Controllers/OrganizerController.cs
@@ -1,6 +1,5 @@
 ﻿using EventFlow_API.Commands;
-using EventFlow_API.Models;
-using EventFlow_API.Repository.Interfaces;
+using EventFlow_API.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 
@@ -8,7 +7,7 @@ namespace EventFlow_API.Controllers;
 
 [Route("organizer")]
 [ApiController]
-public class OrganizerController(IOrganizerRepository organizerRepository) : ControllerBase
+public class OrganizerController(IOrganizerService organizerService) : ControllerBase
 {
 
     [HttpPost]
@@ -16,19 +15,11 @@ public class OrganizerController(IOrganizerRepository organizerRepository) : Con
     {
         try
         {
-            var organizer = new Organizer
-            {
-                Name = organizerCommand.Name,
-                Email = organizerCommand.Email
-            };
+            var organizer = await organizerService.CreateAsync(organizerCommand);
 
-            var create = await organizerRepository.PostAsync(organizer);
-
-            if (create != null)
-                return Ok(create);
-
-            else
-                return BadRequest();
+            return organizer != null ? 
+                Ok(organizer) : 
+                BadRequest();
         }
         catch (SqlException error)
         {
@@ -52,20 +43,10 @@ public class OrganizerController(IOrganizerRepository organizerRepository) : Con
     {
         try
         {
-            var organizerId = await organizerRepository.GetOrganizerByIdAsync(id);
-            var organizer = new Organizer
-            {
-                Name = organizerCommand.Name,
-                Email = organizerCommand.Email
-            };
-
-            var update = await organizerRepository.UpdateAsync(organizer);
-
-            if (update != null)
-                return Ok(update);
-
-            else
-                return NotFound();
+            var updated = await organizerService.UpdateAsync(id, organizerCommand);
+            return updated != null ? 
+                Ok(updated) : 
+                NotFound();
         }
         catch (SqlException error)
         {
@@ -89,13 +70,10 @@ public class OrganizerController(IOrganizerRepository organizerRepository) : Con
     {
         try
         {
-            var delete = await organizerRepository.DeleteAsync(id);
-
-            if (delete > 0)
-                return Ok(delete);
-
-            else
-                return NotFound();
+            var deleted = await organizerService.DeleteAsync(id);
+            return deleted ? 
+                Ok(id) : 
+                NotFound();
         }
         catch (SqlException error)
         {
@@ -119,13 +97,10 @@ public class OrganizerController(IOrganizerRepository organizerRepository) : Con
     {
         try
         {
-            var result = await organizerRepository.GetOrganizerByIdAsync(id);
-
-            if (result != null)
-                return Ok(result);
-
-            else
-                return NotFound();
+            var result = await organizerService.GetByIdAsync(id);
+            return result != null ? 
+                Ok(result) : 
+                NotFound();
         }
         catch (SqlException error)
         {
@@ -149,13 +124,10 @@ public class OrganizerController(IOrganizerRepository organizerRepository) : Con
     {
         try
         {
-            var result = await organizerRepository.GetAllOrganizersAsync();
-
-            if (result != null)
-                return Ok(result);
-
-            else
-                return NotFound();
+            var result = await organizerService.GetAllAsync();
+            return result.Count != 0 ? 
+                Ok(result) : 
+                NotFound();
         }
         catch (SqlException error)
         {
