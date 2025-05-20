4ce9365172f971bfd4631ad01097cdf7e29a9fba
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-05-20 15:24:01 -0300

    ✅feat: add SpeakerCommandValidator with rules for name, email, biography & event association

---
+++ Validators/SpeakerCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using EventFlow_API.Commands;
+
+namespace EventFlow_API.Validators;
+
+public class SpeakerCommandValidator : AbstractValidator<SpeakerCommand>
+{
+    public SpeakerCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("O nome do palestrante é obrigatório.")
+            .MaximumLength(200).WithMessage("O nome deve ter no máximo 200 caracteres.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("O e-mail do palestrante é obrigatório.")
+            .EmailAddress().WithMessage("O e-mail informado não é válido.")
+            .MaximumLength(150).WithMessage("O e-mail deve ter no máximo 150 caracteres.");
+
+        RuleFor(x => x.Biography)
+            .NotEmpty().WithMessage("A biografia do palestrante é obrigatória.")
+            .MaximumLength(2000).WithMessage("A biografia deve ter no máximo 2000 caracteres.");
+
+        RuleFor(x => x.EventId)
+            .GreaterThan(0).WithMessage("O ID do evento é obrigatório e deve ser maior que zero.");
+    }
+}
