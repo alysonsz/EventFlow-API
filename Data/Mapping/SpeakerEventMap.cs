2f93baa782c5fed380f8b88cb4e97e9c575e80f9
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-05-22 16:27:16 -0300

    ✨feat: define SpeakerEvent join entity with composite key
    - Added table mapping for SpeakerEvent with composite PK (SpeakerId, EventId).
    - Configured bidirectional navigation properties.

---
+++ Data/Mapping/SpeakerEventMap.cs
@@ -0,0 +1,26 @@
+﻿using EventFlow_API.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventFlow_API.Data.Mapping;
+
+public class SpeakerEventMap : IEntityTypeConfiguration<SpeakerEvent>
+{
+    public void Configure(EntityTypeBuilder<SpeakerEvent> builder)
+    {
+        builder.ToTable("SpeakerEvent");
+
+        builder.HasKey(se => new { se.SpeakerId, se.EventId });
+
+        builder.Property(se => se.RegisteredAt)
+            .IsRequired()
+            .HasColumnType("DATETIME");
+
+        builder.HasOne(se => se.Speaker)
+            .WithMany(s => s.SpeakerEvents)
+            .HasForeignKey(se => se.SpeakerId);
+
+        builder.HasOne(se => se.Event)
+            .WithMany(e => e.SpeakerEvents)
+            .HasForeignKey(se => se.EventId);
+    }
+}
