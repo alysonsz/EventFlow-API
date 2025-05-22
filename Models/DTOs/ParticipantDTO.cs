767436fde5a37e255e0cb8e4c23060f77699f278
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-05-22 16:57:25 -0300

    ✨feat: create ParticipantDTO for API responses
    - Added ParticipantDTO with essential participant fields.
    - Designed for clean and minimal output in event participation contexts.

---
+++ Models/DTOs/ParticipantDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EventFlow_API.Models.DTOs;
+
+public class ParticipantDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
+
