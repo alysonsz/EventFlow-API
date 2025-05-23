9d3a14eed0acb1cecbf46c35ebbf0b7b1ce85a6b
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-05-23 15:37:09 -0300

    🔧chore: configure JWT authentication in Program.cs
    - Add JWT bearer setup with issuer, audience & key validation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using EventFlow_API.Config;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +10,30 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextConfig(builder.Configuration);
 builder.Services.AddDependencyInjectionConfig();
 
+builder.Services
+    .AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
