c1ba9c9abbcdff476857a9d5cd0f347d725ac8d1
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2025-05-26 10:17:58 -0300

    🔐feat: add ChangePasswordAsync & GetAuthenticatedUserAsync endpoints
    Introduced [Authorize] secured routes for user authentication and management:
    - PUT /auth/change-password: Allows authenticated users to change their password with current validation.
    - GET /auth/data: Returns data of the currently authenticated user.
    
    Improves user experience by providing secure password updates and easy access to user details.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,7 +1,11 @@
 ﻿using EventFlow_API.Commands;
+using EventFlow_API.DTOs;
+using EventFlow_API.Models.DTOs;
 using EventFlow_API.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using System.Security.Claims;
 
 namespace EventFlow_API.Controllers;
 
@@ -47,6 +51,66 @@ public class AuthController(IAuthService authService) : Controller
                 return Unauthorized(new[] { "E-mail ou senha inválidos. " });
 
             return Ok(new { token });
+        }
+        catch (SqlException error)
+        {
+            return StatusCode(500,
+                new[] { "Não foi possível conectar ao banco de dados, por favor tente mais tarde", error.Message });
+        }
+        catch (DbUpdateException error)
+        {
+            return StatusCode(500,
+                new[] { "Algo de errado aconteceu ao salvar, por favor tente mais tarde", error.Message });
+        }
+        catch (Exception error)
+        {
+            return StatusCode(500,
+                new[] { error.Message });
+        }
+    }
+
+    [Authorize]
+    [HttpPut("change-password")]
+    public async Task<IActionResult> ChangePasswordAsync([FromBody] UserPasswordUpdateDTO dto)
+    {
+        try
+        {
+            var result = await authService.UpdatePasswordAsync(User, dto);
+
+            if (!result)
+                return BadRequest("Senha atual incorreta ou usuário inválido.");
+
+            return Ok("Senha atualizada com sucesso.");
+        }
+        catch (SqlException error)
+        {
+            return StatusCode(500,
+                new[] { "Não foi possível conectar ao banco de dados, por favor tente mais tarde", error.Message });
+        }
+        catch (DbUpdateException error)
+        {
+            return StatusCode(500,
+                new[] { "Algo de errado aconteceu ao salvar, por favor tente mais tarde", error.Message });
+        }
+        catch (Exception error)
+        {
+            return StatusCode(500,
+                new[] { error.Message });
+        }
+    }
+
+    [Authorize]
+    [HttpGet("data")]
+    public async Task<IActionResult> GetAuthenticatedUserAsync()
+    {
+        try
+        {
+            var userDto = await authService.GetAuthenticatedUserAsync(User);
+
+            if (userDto == null)
+                return Unauthorized("Usuário não encontrado ou token inválido.");
+
+            return Ok(userDto);
         }
         catch (SqlException error)
         {
