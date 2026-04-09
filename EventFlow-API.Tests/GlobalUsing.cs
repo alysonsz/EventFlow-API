f38e0219769c1cde0d885880075169b849e378c6
Alyson Souza Carregosa <alysonsouza36@gmail.com>
2026-04-09 17:16:25 -0300

    fix: correct GlobalUsing.cs namespaces
    Remove invalid namespaces:
    - EventFlow.Core.Commands (doesn't exist)
    - EventFlow.Core.Models.DTOs (doesn't exist)
    - EventFlow.Core.Services.Interfaces (doesn't exist)
    
    Remove duplicates:
    - EventFlow.Core.Models
    - EventFlow.Application.Services
    - EventFlow.Application.Validators

--- EventFlow-API.Tests/GlobalUsing.cs
+++ EventFlow-API.Tests/GlobalUsing.cs
@@ -5,17 +5,11 @@ global using FluentAssertions;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Mvc;
 
-global using EventFlow.Core.Commands;
-global using EventFlow.Core.Models;
-global using EventFlow.Core.Models.DTOs;
-global using EventFlow.Core.Services.Interfaces;
 global using EventFlow.Core.Models;
 global using EventFlow.Core.Primitives;
 global using EventFlow.Core.Repository;
 global using EventFlow.Core.ValueObjects;
 
-global using EventFlow.Application.Services;
-global using EventFlow.Application.Validators;
 global using EventFlow.Application.Abstractions;
 global using EventFlow.Application.Commands;
 global using EventFlow.Application.DTOs;
